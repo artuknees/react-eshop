d6ec93cf31c3d8fe4cd7218906e4d49439bde2cf
artuknees <artuknees@gmail.com>
2022-06-05 20:22:36 -0300

    Added all react components, containers, style files and pages - working

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { BrowserRouter , Routes , Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
-import Login from '../containers/Login';
-import RecoveryPassword from '../containers/RecoveryPassword';
-import './../styles/global.css'; // importo los estilos
 import Home from '../pages/Home';
+import Login from '../pages/Login';
+import PasswordRecovery from '../pages/PasswordRecovery';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
 import NotFound from '../pages/NotFound';
+import './../styles/global.css'; // importo los estilos
 
 const App = () => {
     return (
@@ -14,7 +20,13 @@ const App = () => {
                 <Routes>
                     <Route exact path="/" element={<Home />} />
                     <Route exact path="/login" element={<Login />} />
-                    <Route exact path="/RecoveryPassword" element={<RecoveryPassword />} />
+                    <Route exact path="/PasswordRecovery" element={<PasswordRecovery />} />
+                    <Route exact path="/SendEmail" element={<SendEmail/>} />
+                    <Route exact path="/NewPassword" element={<NewPassword/>} />
+                    <Route exact path="/MyAccount" element={<MyAccount/>} />
+                    <Route exact path="/CreateAccount" element={<CreateAccount/>} />
+                    <Route exact path="/Checkout" element={<Checkout/>} />
+                    <Route exact path="/Orders" element={<Orders/>} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </Layout>
