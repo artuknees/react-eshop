60333d51cbc25a1a09ec2ac5d98a2e1c3db81c82
artuknees <artuknees@gmail.com>
2022-06-14 09:09:22 -0300

    Generated home with API called objects

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import '@styles/ProductItem.scss';
 import addToCart from '@icons/bt_add_to_cart.svg';
 
-const ProductItem = () => {
-	const [cart , setCart] = useState([]); // el estado y una funcion modificadora del estado
+const ProductItem = ({product}) => { // lo hago recibir algo para poder iterar.
+	const [cart , setCart] = useState([]); 
 	
 	const handleClick = () => {
 		setCart([]);
@@ -11,11 +11,11 @@ const ProductItem = () => {
 
 	return (
 		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+			<img src={product.images[0]} alt={product.title} />
 			<div className="product-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>${product.price}</p>
+					<p>{product.title}</p>
 				</div>
 				<figure onClick={handleClick}>
 					<img src={addToCart} alt="" />
